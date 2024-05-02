502c4f6820a2795beffa21ba2ae70cd32e1b8a79
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-05-02 15:14:06 +0200

    No more bugs when moving the spaceship

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SpaceShip from "./components/SpaceShip"
 
 
@@ -7,22 +7,33 @@ export default function App() {
   const [xPos,setXPos]=useState<number>(1);
   const [yPos,setYPos]=useState<number>(1);
 
-  document.addEventListener('keydown',(event)=>{
-    switch(event.key){
-      case "ArrowUp":
-        yPos>1? setYPos(yPos-1):setYPos(1);
-        break;
-      case "ArrowDown":
-        yPos<8? setYPos(yPos+1):setYPos(8);
-        break;
-      case "ArrowLeft":
-        xPos>1?setXPos(xPos-1):setXPos(1);
-        break;
-      case "ArrowRight":
-        xPos<8?setXPos(xPos+1):setXPos(8);
-        break;
-    }
-  })
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowUp":
+          yPos > 1 ? setYPos(yPos - 1) : setYPos(1);
+          break;
+        case "ArrowDown":
+          yPos < 8 ? setYPos(yPos + 1) : setYPos(8);
+          break;
+        case "ArrowLeft":
+          xPos > 1 ? setXPos(xPos - 1) : setXPos(1);
+          break;
+        case "ArrowRight":
+          xPos < 8 ? setXPos(xPos + 1) : setXPos(8);
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Retirer l'écouteur d'événements lorsque le composant est démonté
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [xPos, yPos]);
 
   return (
     <div id="app"  >
