1dca51623e56399555e796b292c34671e7b3353c
Orlane <148352870+OrlaneB@users.noreply.github.com>
2024-05-06 17:05:17 +0200

    You can make lights disappear with the spaceship

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,81 @@
 // import React from 'react'
-// import { useState } from "react";
+import { useState } from "react";
 import SpaceShip from "./components/SpaceShip"
 import Lights from "./components/Lights";
 // import { useEffect } from "react";
 
 
 export default function App() {
+  //TYPES
+  //For the object with the position of SpaceShip
+  type PosObj = {xPos:number,yPos:number};
 
-    type PosObj = {xPos:number,yPos:number};
-    type LightObj = {
-      id:number;
-      xPos:number;
-      yPos:number
+  //For every light object
+  type LightObj = {
+    id:number;
+    xPos:number;
+    yPos:number
   }
-  type updateListCb = (arg0:LightObj[])=>void;
   
+  //For the cb function that updates the array list of lights
+  type updateListCb = (arg0:LightObj[])=>void;
 
+
+  //STATES
+  const [listLights,setListLights] = useState<LightObj[]>();
+
+  
+  
+  //FUNCTIONS
     let pos:PosObj={'xPos':1,'yPos':1};
-    let listLights:LightObj[]|any = [];
 
     function changePosSpaceShip(value:PosObj){
       pos=value;
       checkIfSpaceShipMeetsLight()
     }
 
-    const newListLight:updateListCb =function updateListLights(value:LightObj[]){
-      listLights=value;
-      console.log(listLights)
+    const newListLight:updateListCb = function updateListLights(value:LightObj[]){
+      setListLights(value);
     }
 
 
 
-    function checkIfSpaceShipMeetsLight (){
+    async function checkIfSpaceShipMeetsLight (){
+
       let shipX:number = pos.xPos;
       let shipY:number = pos.yPos;
 
-        for(let i=0;i<listLights.length;i++){
-          let lightX:number = listLights[i].yPos;
-          let lightY:number = listLights[i].xPos;
 
-          // console.log(`Ship : ${shipX}/${shipY} ; Light : ${lightX}/${lightY}`);
-            if(shipX===lightX && shipY===lightY){
-              console.log("boom!")
-            }
-        }
+      for(let i = 0; i < (listLights ?? []).length; i++){
+        if (listLights && listLights[i]) {
+          let lightX: number = listLights[i].yPos;
+          let lightY: number = listLights[i].xPos;
+
+          if (shipX === lightX && shipY === lightY) {
+              console.log("boom!",`Touched light ${i+1}`);
+              spaceshipTouchesLight(i);
+          }
+      }
     }
 
-  
+    }
+
+    function spaceshipTouchesLight(indexLight:number):void{
+      //Remove the light
+      if(listLights !== undefined){
+        const newList = [...listLights];
+        newList.splice(indexLight,1);
+        setListLights(newList);
+      }
+      
+      //Add a point
+    }
 
 
   return (
     <div id="app"  >
       <SpaceShip updatePosition={(value:PosObj)=>changePosSpaceShip(value)}/>
-      <Lights updateListCb={newListLight}/>
+      <Lights updateListCb={newListLight} listLights={listLights}/>
     </div>
   )
 }
--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 // import { useState } from "react"
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import "../style/Lights.css"
 
 type LightObj = {
@@ -18,50 +18,62 @@ type LightObj = {
 
 
 interface Props {
-    updateListCb:updateListCb
+    updateListCb:updateListCb,
+    listLights?:LightObj[]
 }
 
 
-export default function Lights({updateListCb }: Props) {
+export default function Lights({updateListCb,listLights=[] }: Props) {
     // const[step,setStep]=useState<number>(1);
-    let listLights:LightObj[]=[];
     let nbLights:number = 6;
 
 
     function createLights(){
+        const newLights: LightObj[] = [];
         //for loop to create the lights
         //randomly put them somewhere in the grid
         //But not somewhere a light alredy is
+        // console.log("Create Lights is called");
         for(let i=1;i<=nbLights;i++){
             let xPos = Math.floor(Math.random() * 8)+1;
             let yPos = Math.floor(Math.random() * 8)+1;
-
+            // console.log(`Light ${i} : `)
+            // console.log(`current light : ${xPos},${yPos}`)
                 
-                for(let j=0;j<listLights.length;j++){
-                    
-                    if(listLights[j].xPos === xPos && listLights[j].yPos === yPos){
-                        // console.log(j)
-                        // console.log(`Before : ${xPos},${yPos}`);
-                        xPos = Math.floor(Math.random() * 8)+1;
-                        yPos = Math.floor(Math.random() * 8)+1;
-                        j=0;
-                        // console.log(`After : ${xPos},${yPos}`);
+                for(let j=0;j<newLights.length;j++){
+                    // console.log(newLights[j]);
+                    // console.log(newLights[j].yPos);
+                    if(newLights[j]){
+                        if(newLights[j].xPos === xPos){
+                            if(newLights[j].yPos===yPos){
+                                // console.log(j)
+                                // console.log(`Before : ${xPos},${yPos}`);
+                                xPos = Math.floor(Math.random() * 8)+1;
+                                yPos = Math.floor(Math.random() * 8)+1;
+                                j=0;
+                                // console.log(`After : ${xPos},${yPos}`);
+                            }
+                        }
                     }
+                    
                 }
           
-            listLights.push({id:i,'xPos':xPos,'yPos':yPos})
+            newLights.push({id:i,'xPos':xPos,'yPos':yPos})
             
     
         }
-        updateListCb(listLights);
+        return(newLights)
 
     //    for (let i = 0; i < listLights.length; i++) {
     //     console.log(`My light is positioned at: ${listLights[i].xPos} : ${listLights[i].yPos}`);
     // }
     }
 
-    createLights();
-
+    useEffect(()=>{
+        const lights = createLights();
+        updateListCb(lights);
+    },[])
+   
     
     
     // checkIfSpaceShipMeetsLight();
@@ -69,9 +81,10 @@ export default function Lights({updateListCb }: Props) {
 
   return (
     <div id="lightsDiv">
-       {listLights.map((light)=>(
+       {
+       listLights.length>=1 &&(listLights.map((light)=>(
             <div key={light.id} style={{gridRowStart:light.xPos,gridColumnStart: light.yPos}} className="light">{light.id}</div>
-       ))}
+       )))}
     </div>
   )
 }
